eaaa33a4bfb88e0570bbe1eda32110d1aed8f328
pedro <ph8464803@gmail.com>
2021-02-15 16:13:20 -0300

    cadasto dos produtos

--- admin/db.php
+++ admin/db.php
@@ -9,7 +9,6 @@ function produto_get_data($redirectOnError): array
     $price_share = filter_input(INPUT_POST, 'price_share');
     $image = filter_input(INPUT_POST, 'image');
 
-//    var_dump($name,$description,$price,$category_id,$price_share, $image);
     if (is_null($name)) {
         header('location:' . $redirectOnError);
         die();
@@ -21,10 +20,6 @@ function produto_get_data($redirectOnError): array
 $CreatProduct = function () use ($conn) {
     $data = produto_get_data('/admin/produto');
 
-    echo '<pre>';
-    var_dump($data);
-    echo '</pre>';
-
     $sql = 'INSERT INTO produtos (name, description, category_id, price, price_share, image) VALUE (:name, :description, :category_id, :price, :price_share, :image);';
 
     $stmt = $conn->prepare($sql);
@@ -35,9 +30,7 @@ $CreatProduct = function () use ($conn) {
     $stmt->bindParam(':price', $data['price']);
     $stmt->bindParam(':price_share', $data['price_share']);
 
-
     return $stmt->execute();
-
 };
 
 
