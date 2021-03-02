3ce9c2a96955a2f9b98fdf0605163bf0063a255b
pedro <ph8464803@gmail.com>
2021-03-02 14:31:59 -0300

    criado arquivo de exemplo de config do banco

---
+++ ExemploConfig.php
@@ -0,0 +1,11 @@
+<!-Renomear para config.php--->
+<?php
+
+// nome do server do banco
+define('DB_SERVER', '');
+// nome do user do banco
+define('DB_USER', '');
+// password dp banco
+define('DB_PASSWORD', '');
+// nome do banco
+define('DB_NAME', '');
