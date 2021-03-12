24032a308e534b486aca16c70fcf6bc0aa7c15a5
pedro <ph8464803@gmail.com>
2021-03-12 11:41:44 -0300

    :bug: resolvendo bug

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
-Header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , X-Requested-With, Authorization');
-Header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Origin:", "*");
+header('Access-Control-Allow-Credentials:', 'false');
+header('Access-Control-Allow-Headers:', ' cache-control,content-type,content-encoding,expires,vary,server,x-aspnet-version,set-cookie,x-powered-by,date,content-length');
+header("Access-Control-Allow-Methods", 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
 
 
 require __DIR__ . '/../bootstrap.php';
